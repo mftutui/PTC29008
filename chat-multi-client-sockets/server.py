963415267deb48a2eb80035ea561a5f4b0af801f
Maria Fernanda Tutui <tutui@Marias-MacBook-Air.local>
2019-02-23 15:56:01 -0300

    private message

--- chat-multi-client-sockets/server.py
+++ chat-multi-client-sockets/server.py
@@ -9,10 +9,9 @@ def send_to_all (sock, message):
 				socket.close()
 				connected_list.remove(socket)
 
-def private_msg(sock):
-	sock.send("\r\33[31m\33[1mWrong sintaxe! \n To send a private message use:\33[0m \33[34m\33[1m'private;USERNAME:msgContent'\n\33[0m")
+def private_guide(sock):
+	sock.send("\r\33[31m\33[1mWrong sintaxe! \n To send a private message use:\33[0m \33[34m\33[1m'p.USERNAME:msgContent'\n\33[0m")
 
-#check if the name is already on record dict
 def check_name_list():
 	if name not in record.values():
 		record[addr] = name
@@ -22,7 +21,7 @@ def check_name_list():
 		print "Client (%s, %s) connected" % addr,"[",record[addr],"]" 
 
 		conn.send("\33[32m\r\33[1m Welcome to chat room. Enter 'exit' anytime to left. \n\33[0m") 
-		send_to_all(conn, "\33[32m\r\33[1mThe user \33[34m\33[1m"+name+"\33[0m \33[32m\33[1mjoined the conversation. \n\33[0m")
+		send_to_all(conn, "\33[32m\r\33[1m The user \33[34m\33[1m"+name+"\33[0m \33[32m\33[1mjoined the conversation. \n\33[0m")
 	else:
 		conn.send("\r\33[31m\33[1mUsername already taken! \n\33[0m")
 		del record[addr]
@@ -38,6 +37,23 @@ def show_users(sock, suport, your_name):
 				sock.send("\33[32m\r\33[1m - "+id_name+"\n\33[0m")
 	else:
 		sock.send("\33[31m\r\33[1mThere are no users connected besides you.\n\33[0m")
+
+def private_msg(data, sock, suport):
+	i = 0
+	if (data.find(".") != -1) and (data.find(":") != -1):
+		name_p = data[data.find(".")+1:data.find(":")]
+		for addr, id_name in suport.items():
+			if str(id_name) == str(name_p):
+				addr_p = addr
+				i = 1
+		if i == 1:
+			msg = data[data.find(":")+1:]
+			print "Client (%s, %s) " % (ip,port),"[",record[(ip,port)],"] sending a private message to: [ %s ]" % name_p
+			addr_p[1].send("\33[34m\r\33[1mPrivate message from: "+record[(ip,port)]+"\33[0m "+msg+ "\n")	
+		else:
+			sock.send("\33[31m\r\33[1m The user \33[34m\33[1m"+name_p+"\33[0m \33[31m\33[1mis not connected. \n\33[0m")
+	else:
+		private_guide(sock)
 
 		
 if __name__ == "__main__":
@@ -82,12 +98,7 @@ if __name__ == "__main__":
 				try:
 					data_r = sock.recv(buffer)
 					data = data_r[:data_r.index("\n")]
-					
-                    #geting from client sending the message
 					ip,port = sock.getpeername()
-
-
-					#server status msg
 					print "Data [ %s ] received from:" % data,"[",record[(ip,port)],"]" 
 
 					if data == "exit":
@@ -102,27 +113,28 @@ if __name__ == "__main__":
 						connected_list.remove(sock)
 						sock.close()
 						continue
-											
-					# private message
-					elif "private" in data:
-						if data == "private":
-							private_msg(sock)
-						else:
-							if (data.find(";") != -1) and (data.find(":") != -1):
-								name_p = data[data.find(";")+1:data.find(":")]
-								for addr, id_name in suport.items():
-									if str(id_name) == name_p:
-										msg = data[data.find(":")+1:]
-										#server status msg
-										print "Client (%s, %s) " % (ip,port),"[",record[(ip,port)],"] sending a private message to: [ %s ]" % name_p
-										addr[1].send("private;"+record[(ip,port)]+":"+msg)
-										continue	
-									else:
-										sock.send("\r\33[1m"+"\33[31mThe user "+addr[1]+" is not connected\n\33[0m")
-										continue
-								continue	
-							else:
-								private_msg(sock)
+
+					elif data == "private":
+						private_guide(sock)
+
+					elif "p." in data:
+						private_msg(data, sock, suport)
+						#if (data.find(";") != -1) and (data.find(":") != -1):
+							#name_p = data[data.find(";")+1:data.find(":")]
+							#for addr, id_name in suport.items():
+								#if str(id_name) == name_p:
+									#msg = data[data.find(":")+1:]
+									#server status msg
+									#print "Client (%s, %s) " % (ip,port),"[",record[(ip,port)],"] sending a private message to: [ %s ]" % name_p
+									#addr[1].send("private;"+record[(ip,port)]+":"+msg)
+									#continue	
+								#else:
+									#sock.send("\r\33[1m"+"\33[31mThe user "+addr[1]+" is not connected\n\33[0m")
+									#continue
+							#continue	
+						#else:
+							#private_msg(sock)
+
 					elif data == "show users":
 						show_users(sock, suport, record[(ip,port)])
 					else:
@@ -130,6 +142,7 @@ if __name__ == "__main__":
 						send_to_all(sock,msg)
                 # crt+c exit
 				except:
+					print "-------------------------DEU MERDA-------------------------"
 					(ip,port) = sock.getpeername()
 					send_to_all(sock, "\r\33[31m \33[1m"+record[(ip,port)]+" left the conversation unexpectedly\33[0m\n")
 
