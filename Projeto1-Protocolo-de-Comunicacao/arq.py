700d575d8baf6acf6bd0ccf91ea72c6a11fc327d
Paulo Sell <sellpaulof@gmail.com>
2019-04-01 11:40:06 -0300

    atualizando arq

--- Projeto1-Protocolo-de-Comunicacao/arq.py
+++ Projeto1-Protocolo-de-Comunicacao/arq.py
@@ -24,7 +24,32 @@ class ARQ(poller.Layer):
         self.M = 0
         self._quadro = bytearray()
         self._dado = None
+        self.flag = 0
+    
+    def sendACK0(self):
+        quadro_aux = bytearray()
+        quadro_aux.append(self.ack_0)
+        quadro_aux.append(self.protocolo)
+        self.sendToLayer(quadro_aux)
+    
+    def sendACK1(self):
+        quadro_aux = bytearray()
+        quadro_aux.append(self.ack_1)
+        quadro_aux.append(self.protocolo)
+        self.sendToLayer(quadro_aux)
+         
 
+
+
+    def montaQuadroZero(self,dado):
+        quadro_aux = bytearray()
+        quadro_aux.append(self.dado_0)
+        quadro_aux.append(self.protocolo)
+        for i in range (len(dado)):
+            quadro_aux.append(int.from_bytes(dado[i].encode('ascii'),'big'))
+            
+        self.sendToLayer(quadro_aux)
+        print ("saiu do monta")       
         
 
     def setBottom(self, bottom):
@@ -37,43 +62,38 @@ class ARQ(poller.Layer):
 
     def handle(self):
         quadro = sys.stdin.readline()
-        self._dado = quadro[:-1]
-        self.handle_fsm(quadro[:-1])
+        if self.flag != 1:
+            
+            self._dado = quadro[:-1]
+            self.handle_fsm(quadro[:-1])
+            self.flag = 1
+            self.enable_timeout()
+            print ("habilitou timeout")
+        
       
     
-    def montaQuadro(self, dado):
-        print(self._state)
-        self._quadro.clear()
-        if self._state == False:
+    def montaQuadroOne(self, dado):        
+        quadro_aux = bytearray()
+        quadro_aux.append(self.dado_1)
+        quadro_aux.append(self.protocolo)
+        for i in range (len(dado)):
+            quadro_aux.append(int.from_bytes(dado[i].encode('ascii'),'big'))
             
-            self._quadro.append(self.dado_0)
-            self._quadro.append(self.protocolo)
-            for i in range (len(dado)):
-                self._quadro.append(int.from_bytes(dado[i].encode('ascii'),'big'))
-            
-            self.sendToLayer(self._quadro)
-          
-            
-        
-        elif self._state == True:            
-            self._quadro.append(self.dado_1)
-            self._quadro.append(self.protocolo)
-            for i in range (len(dado)):
-                self._quadro.append(int.from_bytes(dado[i].encode('ascii'),'big'))
-            
-            self.sendToLayer(self._quadro)
-        
-        self.enable_timeout()        
-        print("timeout habilitade")
+        self.sendToLayer(quadro_aux) 
+        print ("saiu do monta")       
+               
+       
 
     def _stateZero(self, quadro):
-            self.montaQuadro(quadro)   
+            print("entrou func zero")
+            self.montaQuadroZero(quadro)   
                      
             
            
     
     def _stateOne(self, quadro):
-            self.montaQuadro(quadro)
+            print("entrou func1")
+            self.montaQuadroOne(quadro)
             
             
            
@@ -88,67 +108,50 @@ class ARQ(poller.Layer):
     def handle_timeout(self):
         
         print ("ACK NÃO RECEBIDO...")
-        print ("Enviando ", self._dado)
-        self.montaQuadro(self._dado)
+        print ("Enviando:", self._dado)
+
+        if self._state == False:
+            self.montaQuadroZero(self._dado)
+        elif self._state == True:
+            self.montaQuadroOne(self._dado)
+        
     
     def receiveFromBottom(self, dados):
         if dados[0] == self.dado_0:
             if self.M == 0:
                 print(dados[2:].decode('ascii'))
                 self.M = 1
-                quadro_aux = bytearray()
-                quadro_aux.append(self.ack_0)
-                quadro_aux.append(self.protocolo)                
-                self.sendToLayer(quadro_aux)
-               
+                self.sendACK0()               
             elif self.M == 1:
-                quadro_aux = bytearray()
-                quadro_aux.append(self.ack_0)
-                quadro_aux.append(self.protocolo)                
-                self.sendToLayer(quadro_aux)
+                self.sendACK0()
                 
         elif dados[0] == self.dado_1:
             if self.M == 1:
                 print(dados[2:].decode('ascii'))
                 self.M = 0
-                quadro_aux = bytearray()
-                quadro_aux.append(self.ack_1)
-                quadro_aux.append(self.protocolo)                
-                self.sendToLayer(quadro_aux)
+                self.sendACK1()
             elif self.M == 0:
-                quadro_aux = bytearray()
-                quadro_aux.append(self.ack_1)
-                quadro_aux.append(self.protocolo)                
-                self.sendToLayer(quadro_aux)
+                self.sendACK1()
+
         elif dados[0] == self.ack_0:
             if self._state == False:
                 print("ack 0 recebido ")
                 self.disable_timeout()
-                print("timeout desabilitade")
-                self._quadro.clear()
+                print("timeout desabilitado")
+                self.flag = 0
                 self._state = True
             elif self._state == True:
-                quadro_aux = bytearray()
-                quadro_aux.append(self.dado_0)
-                quadro_aux.append(self.protocolo)
-                for i in range (len(self._dado)):
-                    quadro_aux.append(int.from_bytes(self._dado[i].encode('ascii'),'big'))
-                self.sendToLayer(quadro_aux)
+                self.montaQuadroOne(self._dado)
+               
         
         elif dados[0] == self.ack_1:
             if self._state == True:
                 print("ack 1 recebido ")
                 self.disable_timeout()
-                print("timeout desabilitade")
-                self._quadro.clear()
+                print("timeout desabilitade")                
                 self._state = False
-                
+                self.flag = 0
             elif self._state == False:                
-                quadro_aux = bytearray()
-                quadro_aux.append(self.dado_1)
-                quadro_aux.append(self.protocolo)
-                for i in range (len(self._dado)):
-                    quadro_aux.append(int.from_bytes(self._dado[i].encode('ascii'),'big'))
-                self.sendToLayer(quadro_aux)
+                self.montaQuadroZero(self._dado)
               
 
