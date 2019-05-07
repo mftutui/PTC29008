dd00898395016648d94189f795a6414de3d74bd8
Paulo Sell <sellpaulof@gmail.com>
2019-05-07 01:27:51 -0300

    arredondando ger e arq

--- Projeto1-Protocolo-de-Comunicacao/layer.py
+++ Projeto1-Protocolo-de-Comunicacao/layer.py
@@ -130,7 +130,7 @@ class Protocolo():
         self._ger.setBottom(self._arq)
         self._ger.setTop(self._fake)
         self._fake.setBottom(self._ger)
-        self._ger.connRequest()
+        
         self._poller.adiciona(self._enq)
         self._poller.adiciona(self._arq)
         self._poller.adiciona(self._ger)
